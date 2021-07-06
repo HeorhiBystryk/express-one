e53844033f5b12e9c402bca1e039d96593f68646
Heorhi Bystryk <bystrikg@gmail.com>
2021-07-06 22:36:44 +0300

    check linter

--- modules/healthz/controller.js
+++ modules/healthz/controller.js
@@ -1,15 +1,16 @@
 // const { BadRequestException } = require("../../middleware/exception.handler");
 
 function createController({ service }) {
-    const healthzCheck = async (req, res, next) => {
+    const healthzCheck = async (req, res, next) =>              {
         try {
             await service.healthzCheck()
+            console.log();
             // throw new BadRequestException('АЛЯРМА!')
             return res.status(200).json('App is running...')
 
 
 
-        } catch (error) {
+        }              catch (error) {
             next(error)
         }
     }
