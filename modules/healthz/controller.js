647c090cb60c064ef220ddb6c53502315393df09
Heorhi Bystryk <bystrikg@gmail.com>
2021-07-12 20:57:20 +0300

    check prettier with ws

--- modules/healthz/controller.js
+++ modules/healthz/controller.js
@@ -2,6 +2,7 @@ function createController({ service }) {
     const healthzCheck = async (req, res, next) => {
         try {
             await service.healthzCheck()
+
             return res.status(200).json('App is running...')
         } catch (error) {
             next(error)
